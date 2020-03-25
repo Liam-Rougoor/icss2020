c82236ec4d9b5f4644d7e905b770df8ec08be5bb
Liam Rougoor <35850587+Liam-Rougoor@users.noreply.github.com>
2020-03-25 19:42:51 +0100

    IfClauses worden getransformeerd. Body's mogen leeg zijn.

--- startcode/src/main/java/nl/han/ica/icss/ast/IfClause.java
+++ startcode/src/main/java/nl/han/ica/icss/ast/IfClause.java
@@ -1,5 +1,6 @@
 package nl.han.ica.icss.ast;
 
+import nl.han.ica.icss.ast.literals.BoolLiteral;
 import nl.han.ica.icss.ast.types.ExpressionType;
 import nl.han.ica.icss.checker.VariableStore;
 
@@ -12,7 +13,8 @@ public class IfClause extends ASTNode {
     public Expression conditionalExpression;
     public ArrayList<ASTNode> body = new ArrayList<>();
 
-    public IfClause() { }
+    public IfClause() {
+    }
 
     public IfClause(Expression conditionalExpression, ArrayList<ASTNode> body) {
 
@@ -24,6 +26,7 @@ public class IfClause extends ASTNode {
     public String getNodeLabel() {
         return "If_Clause";
     }
+
     @Override
     public ArrayList<ASTNode> getChildren() {
         ArrayList<ASTNode> children = new ArrayList<>();
@@ -35,13 +38,24 @@ public class IfClause extends ASTNode {
 
     @Override
     public ASTNode addChild(ASTNode child) {
-        if(child instanceof Expression)
-            conditionalExpression  = (Expression) child;
+        if (child instanceof Expression)
+            conditionalExpression = (Expression) child;
         else
             body.add(child);
 
         return this;
     }
+
+    @Override
+    public ASTNode removeChild(ASTNode child) {
+        if(child instanceof Expression){
+            conditionalExpression = null;
+        } else {
+            body.remove(child);
+        }
+        return this;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -64,10 +78,22 @@ public class IfClause extends ASTNode {
     @Override
     public void check(VariableStore<ExpressionType> variableTypes) {
         ExpressionType conditionType = conditionalExpression.getType(variableTypes);
-        if(conditionType != ExpressionType.BOOL){
+        if (conditionType != ExpressionType.BOOL) {
             String errorMessage = "Expression must be of type boolean. ";
             setError(errorMessage);
             conditionalExpression.setError(errorMessage + conditionType);
+        }
+    }
+
+    @Override
+    public void transform(VariableStore<Literal> variableValues, ASTNode parent) {
+        if (((BoolLiteral) conditionalExpression).value) {
+            parent.removeChild(this);
+            for (ASTNode bodyNode : body) {
+                parent.addChild(bodyNode);
+            }
+        } else {
+            parent.removeChild(this);
         }
     }
 }
